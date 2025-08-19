15c28577a47d3f0bddf8fbc5324142d3c87292ee
bashou <bashou@users.noreply.github.com>
2025-08-19 18:09:01 +0200

    chore: remove private data

--- internal/provider/service_ad_insertion_resource_test.go
+++ internal/provider/service_ad_insertion_resource_test.go
@@ -14,9 +14,9 @@ import (
 
 var (
 	SlateURL     = "https://bpkiosamples.s3.eu-west-1.amazonaws.com/broadpeakio-slate.jpg"
-	LiveURL      = "https://hls-radio-s3.nextradiotv.com/olyzon/delayed/master.m3u8"
+	LiveURL      = "https://origin.broadpeak.io/bpk-tv/bpkiofficial/hlsv3/index.m3u8"
 	LiveURLOther = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-	AdServerURL  = "https://vast-prep.staging.olyzon.tv/sources/1042586b/serve"
+	AdServerURL  = "https://bpkiovast.s3.eu-west-1.amazonaws.com/vastmultibpkio"
 )
 
 func TestAccServiceAdInsertion_Basic(t *testing.T) {
